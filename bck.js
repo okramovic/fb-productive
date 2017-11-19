bb8c6ddccfaaa545d5fd7848d6c77b717e231d92
okram ovic <okram@protonmail.ch>
2017-11-19 23:28:59 +0100

    popup loads state from bck.js - for on-off switch & default option to redir to

--- bck.js
+++ bck.js
@@ -2,53 +2,114 @@
   bck pages
   https://developer.chrome.com/extensions/background_pages
 
+      changing ext icon
+      https://stackoverflow.com/questions/16921685/change-the-chrome-extension-icon
+
   tabs
   https://developer.chrome.com/extensions/tabs
 
   loading page detection
-
   https://stackoverflow.com/questions/6497548/chrome-extension-make-it-run-every-page-load
 
 */
 
-var adsToBlock = [
+/**
+ *          to do:
+
+            - when turned on, it checks all open tabs and redirects them
+
+
+
+            done:
+
+ * 
+ */
+
+var adrToBlock = [
                     "https://www.facebook.com/", "https://www.facebook.com", 
                     "http://www.facebook.com/", "http://www.facebook.com"
 
                   ],
 tabsToChange = [],
-desiredURL = "https://www.facebook.com/saved/?cref=28"; 
-            //"https://www.facebook.com/events/"
+
             
+desiredURL = "https://www.facebook.com/saved/?cref=28", //"https://www.facebook.com/groups/" //"https://www.facebook.com/events/"
+redirect = true;
 
+//window.desiredURL = "https://www.facebook.com/saved/?cref=28"
 
-
-// check all tabs when Ext is turned on
-queryTabs();
+//  check all tabs when Ext is turned on - 
+//  doesnt work
+    queryTabs();
+    //alert("wejfwfwm  " + window.desiredURL);
 
 
 chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
     
     //alert("new tab load");
-    
-    let updateProps = {
-                          url: desiredURL 
-                      }
+    if (redirect){
+        if ( adrToBlock.some(matchesTabURL)  ){
+          
+                        //alert("fb homepage open");
+                        //tabsToChange.push(tab.id)
+                        let updateProps = { url: window.desiredURL }
+                        chrome.tabs.update(tab.id, updateProps)//, function(){});
+        }
 
-    if ( adsToBlock.some(matchesTabURL)  ){
-      
-                    //alert("fb homepage open");
-                    //tabsToChange.push(tab.id)
-      
-                    chrome.tabs.update(tab.id, updateProps, function(){});
     }
-
-
 
     function matchesTabURL(adr){
       return adr === tab.url 
     }
 })
+
+chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {
+    //alert("now >> " + request.newChoice);
+    /*console.log(sender.tab ?
+                "from a content script:" + sender.tab.url :
+                "from the extension");*/
+    //alert("request\n" + JSON.stringify(request));
+
+
+    // send to popup if redirection is turned on
+    if (request.onOffState){
+          sendResponse({state: redirect});
+          return
+    }
+
+    // get initial address to redirect to & display in Select pop-up menu
+    if (request.redirectTo){
+          sendResponse({to: desiredURL});
+          return
+    }
+
+    // handling on-off switch from popup
+    if (request.onOff){
+        if (request.onOff == "true") redirect = true
+        else redirect = false
+
+        if (redirect===false)       chrome.browserAction.setIcon( {path: "icon_sm_off.png"}, function callback(){})
+        else if (redirect === true) chrome.browserAction.setIcon( {path: "icon_sm.png"},     function callback(){})
+
+        return
+    }
+
+    if (request.newChoice){
+        switch(request.newChoice){
+          case "saved items": window.desiredURL = "https://www.facebook.com/saved/?cref=28"; break;
+          case "messages":    window.desiredURL = "https://www.facebook.com/messages/"; break;
+          case "events":      window.desiredURL = "https://www.facebook.com/events/"; break;
+          case "groups":      window.desiredURL = "https://www.facebook.com/groups/"; break;
+          case "other":       window.desiredURL = "" ; break;
+          //default: window.desiredURL = "";
+        }
+    }
+    //alert("desiredURL: " + desiredURL)
+    /*  sendResponse({farewell: "goodbye"});
+    */  
+  });
+
+
 
 
 
@@ -57,7 +118,7 @@ function queryTabs(){
   //let p = document.createElement('p')
   //alert(cons);
 
-  //alert("running bck");
+  //alert("query tabs");
   
   //alert(desiredURL);
   //p.innerHTML = desiredURL;
@@ -83,13 +144,13 @@ function queryTabs(){
         //if (url === "https://www.facebook.com/") alert("bad url  " + url)
 
         let updateProps = {
-                url: desiredURL //"https://www.w3schools.com/colors/colors_picker.asp"
+                //url: window.desiredURL //"https://www.w3schools.com/colors/colors_picker.asp"
             }
         
 
-        if ( adsToFilter.some(matchesTabURL)  ){
-              
-                      chrome.tabs.update(tab.id, updateProps, function(){});
+        if ( adrToBlock.some(matchesTabURL)  ){
+                      //alert("fb page open");
+                      //chrome.tabs.update(tab.id, updateProps, function(){});
                       //tabsToChange.push(tab.id)
         }
         
--- popup.js
+++ popup.js
@@ -1,15 +1,60 @@
-// find out if there is any tab that has fcb open
-//alert('hi');
+var onOffState = null, redirectTo = null;
+
+
 
 
 document.addEventListener('DOMContentLoaded', () => {
-    alert('def url' + desiredURL);
+    //alert('def url' + desiredURL);
+    //alert('hi');
 
+
+    // get state of main switcher
+    chrome.runtime.sendMessage({onOffState: 1 }, function(response) {
+
+                onOffState = response.state
+                document.getElementById("onOff").checked = onOffState
     
+    })
+
+    // get initial redirect Choice
+    chrome.runtime.sendMessage({redirectTo: 1 }, function(response) {
+
+          //alert("response\n" + JSON.stringify(response));
+          redirectTo = response.to
+          let opts = document.getElementById("redirectTo").options
+          //alert("redir to");
+          //alert(opts);
+
+          for (var i=0; i<opts.length; i++){
+              if (opts[i].value === response.to) document.getElementById("redirectTo").selectedIndex = i;
+          }
+
+    })
+
+
+    let sel = document.getElementById("redirectTo")
+    //alert("hola " + sel)
+    sel.addEventListener('change', newURLSelection);
+
+
+    //
+    //alert(switcher);
+
+    document.getElementById("onOff").addEventListener('change', onSwitch)
 });
 
-document.addEventListener('change', newURLSelection)
 
+
+function onSwitch(){
+  
+  let switcher = document.getElementById("onOff")
+
+
+  //alert("? " + switcher.checked)
+  chrome.runtime.sendMessage({onOff: switcher.checked.toString() }, function(response) {
+  })
+      
+}
 
 
 
@@ -18,7 +63,13 @@ function newURLSelection(){
   let sel = document.getElementById("redirectTo")
   //alert(sel.options[sel.selectedIndex].value);  // [selectedIndex]
 
-  desiredURL = sel.options[sel.selectedIndex].value
+  desiredURL = sel.options[sel.selectedIndex].text
 
-  alert("des url: " + desiredURL);
+  //alert("des url: " + desiredURL);
+
+  chrome.runtime.sendMessage({newChoice: desiredURL}, function(response) {
+    //console.log(response.farewell);
+  });
+
 }
+
